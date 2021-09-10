da9e9367e5ec2dd8cbb977030ec843481ffb78fb
sneha.n <9sneha.n@gmail.com>
2021-09-10 20:27:29 +0530

    matters-that-matter changes

---
+++ matters.js
@@ -0,0 +1,54 @@
+const article_list = document.getElementById("article-list");
+
+fetch('https://9sneha-n.github.io/nnarayanan/content/matters_that_matters/articles.json')
+    .then( (response) => {
+        console.log("Fetched artcles from server, parsing json" + response);
+        return response.json();
+    })
+    .then((data) => {
+        data.forEach(article => {
+ 
+            let article_img = document.createElement("img");
+            article_img.alt = "article image";
+            article_img.className = "article-img";
+            article_img.src = article.image;
+
+            let article_image = document.createElement("div");
+            article_image.className = "article-image";
+            article_image.appendChild(article_img);
+
+            let article_div = document.createElement("div");
+            article_div.className = "article-div";
+            
+            let article_content = document.createElement("div");
+            article_content.className = "article-content";
+
+            let article_headline = document.createElement("p");
+            article_headline.className = "article-headline";
+            article_headline.textContent = article.title;
+
+            let article_subtitle = document.createElement("p");
+            article_subtitle.className = "article-subtitle";
+            article_subtitle.textContent = article.subtitle;
+
+            let article_read_more = document.createElement("p");
+            article_read_more.className = "article-read-more";
+            article_read_more.textContent = "Click to read full story";
+
+            article_content.appendChild(article_headline);
+            article_content.appendChild(article_subtitle);
+            article_content.appendChild(article_read_more);
+
+            article_div.appendChild(article_image);
+            article_div.appendChild(article_content);
+
+            article_div.onclick = (e, data) => {
+                window.open(article.link);
+            }
+
+            article_list.appendChild(article_div);
+        });
+    })
+    .catch(err => console.log("Error Thrown fetching articles from server" + err));
+
+
