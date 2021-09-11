42d7c560edd41973f9cabdab78d0b75be68af334
sneha.n <9sneha.n@gmail.com>
2021-09-11 12:47:19 +0530

    matters date and link

--- matters.js
+++ matters.js
@@ -9,18 +9,32 @@ fetch('https://9sneha-n.github.io/nnarayanan/content/matters_that_matter/article
         data.forEach(article => {
 
             let article_div = document.createElement("div");
+            article_div.id = article.id;
             article_div.className = "article-div";
 
             let article_headline = document.createElement("p");
             article_headline.className = "article-headline";
-            if (article.title.length) {
-                article_headline.textContent = article.title;
-                article_div.appendChild(article_headline);
-            }
+            article_headline.textContent = article.title;
+            article_div.appendChild(article_headline);
+
+            let article_date = document.createElement("p");
+            article_date.className = "article-subtitle";
+            article_date.textContent = article.date;
+            article_div.appendChild(article_date);
+
             let article_subtitle = document.createElement("p");
             article_subtitle.className = "article-subtitle";
             article_subtitle.innerText = article.subtitle;
             article_div.appendChild(article_subtitle);
+
+            let article_read_more = document.createElement("p");
+            article_read_more.className = "article-read-more";
+            article_read_more.textContent = "Click to copy link to this article";
+            article_div.appendChild(article_read_more);
+
+            article_div.onclick = (e, data) => {
+                navigator.clipboard.writeText(article.link)
+            }
             article_list.appendChild(article_div);
         });
     })
