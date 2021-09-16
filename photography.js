7a924fa2497767bc907cf742bbdea28abeb1c317
sneha.n <9sneha.n@gmail.com>
2021-09-16 12:53:02 +0530

    photos

---
+++ photography.js
@@ -0,0 +1,24 @@
+const photograph_list = document.getElementById("photograph-list");
+
+fetch('https://9sneha-n.github.io/nnarayanan/content/photography/photography.json')
+    .then( (response) => {
+        console.log("Fetched artcles from server, parsing json" + response);
+        return response.json();
+    })
+    .then((data) => {
+        data.forEach(photograph => {
+
+            let photograph_div = document.createElement("div");
+            photograph_div.className = "article-div";
+
+
+            let phtograph_img = document.createElement("img");
+            phtograph_img.src = photograph.image;
+            photograph_div.appendChild(phtograph_img);
+
+            photograph_list.appendChild(photograph_div);
+        });
+    })
+    .catch(err => console.log("Error Thrown fetching photographs from server" + err));
+
+
